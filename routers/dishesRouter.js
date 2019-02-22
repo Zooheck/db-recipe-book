dfb4ce48606233c9124eeec91971b3478d60e5e9
Zooheck <davidscottflack@gmail.com>
2019-02-21 19:05:30 -0600

    Get dish by id

--- routers/dishesRouter.js
+++ routers/dishesRouter.js
@@ -14,6 +14,18 @@ router.get('/', async (req, res) => {
     }
 })
 
-
+router.get('/:id', async (req, res) => {
+    const dish = await DishFuncs.getById(req.params.id)
+    try {
+        if(dish) {
+            res.status(200).json(dish)
+        } else {
+            res.status(404).json({message: 'Dish not found'})
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: 'Error retrieving dish'})
+    }
+})
 
 module.exports = router;
